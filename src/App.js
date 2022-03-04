3fdbf1a20693773e765feef2ce05131ec04e2ed4
thomasalemayehu <61502533+thomasalemayehu@users.noreply.github.com>
2022-03-04 17:57:31 +0300

    Landing Page Complete Ver 1.0 : Add button Hover animation

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Body from "./app/Body";
 import Navigation from "./app/Navigation";
 
 function App() {
+  document.body.style.zoom = "100%";
   return (
     <section className="app">
       <Navigation />
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -7,12 +7,14 @@ function LandingPage() {
     <section className="landing__page__container" id="LandingPage">
       {/* Left Section */}
       <div className="landing__page__left">
-        <p>Howdy-doody! 🙃 I'm</p>
-        <h1>Thomas Alemayehu</h1>
+        <p>Howdy-doody!🙃 I'm</p>
+        <h1>Thomas Alemayehu.</h1>
         <h5>Full Stack developer & Security Analyst</h5>
         <p>
-          I am a full stack developer with great eye for design and optimized
-          backend based in Addis Ababa.
+          I’m a software engineer specializing in building (and occasionally
+          designing) exceptional digital experiences. Currently, I’m focused on
+          building accessible, human-centered products at Addis Ababa Institute
+          of Technology.
         </p>
         <a href="#ContactPage">Hire Me</a>
       </div>
@@ -20,7 +22,7 @@ function LandingPage() {
       {/* Right Section */}
 
       <img
-        src="/assets/image/landing-page-image3.png"
+        src="/assets/image/landing-page-image.png"
         className="landing__page__right"
         alt="A fun animation of me"
       />
