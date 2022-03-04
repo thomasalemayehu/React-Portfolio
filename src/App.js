be2f9f518af2697da0fe96cbeb0faef5444c3d44
thomasalemayehu <61502533+thomasalemayehu@users.noreply.github.com>
2022-03-04 18:01:16 +0300

    Bug Fix

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Body from "./app/Body";
 import Navigation from "./app/Navigation";
 
 function App() {
-  document.body.style.zoom = "100%";
+  document.body.style.zoom = "80%";
   return (
     <section className="app">
       <Navigation />
